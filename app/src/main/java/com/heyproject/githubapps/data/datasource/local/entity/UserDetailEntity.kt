7c5c21291dea2db7b21a05b71043d91625f85887
yayanrw <yayanraw@gmail.com>
2022-11-11 01:03:27 +0700

    add UserDetailEntity.kt

---
+++ app/src/main/java/com/heyproject/githubapps/data/datasource/local/entity/UserDetailEntity.kt
@@ -0,0 +1,25 @@
+package com.heyproject.githubapps.data.datasource.local.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+Written by Yayan Rahmat Wijaya on 11/11/2022 00:43
+Github : https://github.com/yayanrw
+ **/
+
+@Entity(tableName = "user_detail")
+data class UserDetailEntity(
+    @PrimaryKey val id: Int,
+    val login: String,
+    val name: String?,
+    val bio: String?,
+    val blog: String?,
+    val company: Any?,
+    @ColumnInfo(name = "avatar_url") val avatarUrl: String?,
+    val followers: Int?,
+    val following: Int?,
+    @ColumnInfo(name = "public_repos") val publicRepos: Int?,
+    val isFavorite: Boolean = false
+)
