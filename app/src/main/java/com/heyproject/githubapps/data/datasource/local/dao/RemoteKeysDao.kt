0eeea225239770c81f0eca934231a6fd128a5920
yayanrw <yayanraw@gmail.com>
2022-11-11 01:20:28 +0700

    add RemoteKeysDao.kt

---
+++ app/src/main/java/com/heyproject/githubapps/data/datasource/local/dao/RemoteKeysDao.kt
@@ -0,0 +1,24 @@
+package com.heyproject.githubapps.data.datasource.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.heyproject.githubapps.data.datasource.local.entity.RemoteKeysEntity
+
+/**
+Written by Yayan Rahmat Wijaya on 11/11/2022 00:40
+Github : https://github.com/yayanrw
+ **/
+
+@Dao
+interface RemoteKeysDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(remoteKey: List<RemoteKeysEntity>)
+
+    @Query("SELECT * FROM remote_keys WHERE id = :id")
+    suspend fun getRemoteKeysId(id: String): RemoteKeysEntity?
+
+    @Query("DELETE FROM remote_keys")
+    suspend fun deleteRemoteKeys()
+}
