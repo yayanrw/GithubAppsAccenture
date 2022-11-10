6abd24d83f79b4ee89b901a9b0314bd180de1f85
yayanrw <yayanraw@gmail.com>
2022-11-10 23:27:41 +0700

    add GithubService.kt

---
+++ app/src/main/java/com/heyproject/githubapps/data/datasource/remote/api/GithubService.kt
@@ -0,0 +1,43 @@
+package com.heyproject.githubapps.data.datasource.remote.api
+
+import com.heyproject.githubapps.common.FollowType
+import com.heyproject.githubapps.data.datasource.remote.dto.UserDetailDto
+import com.heyproject.githubapps.data.datasource.remote.dto.UserDto
+import com.heyproject.githubapps.data.datasource.remote.response.UserSearchResponse
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+/**
+Written by Yayan Rahmat Wijaya on 11/10/2022 23:02
+Github : https://github.com/yayanrw
+ **/
+
+interface GithubService {
+    @GET("users")
+    suspend fun getUsers(
+        @Header("Authorization") auth: String,
+        @Query("since") since: Int? = 0,
+        @Query("perPage") perPage: Int = 10,
+    ): List<UserDto>
+
+    @GET("users/{userName}")
+    suspend fun getUserDetail(
+        @Header("Authorization") auth: String,
+        @Path("userName") username: String,
+    ): UserDetailDto
+
+    @GET("search/users")
+    suspend fun getSearchUsers(
+        @Header("Authorization") auth: String,
+        @Query("q") query: String,
+    ): UserSearchResponse
+
+    @GET("users/{userName}/{followType}")
+    suspend fun getUserFollow(
+        @Header("Authorization") auth: String,
+        @Path("userName") username: String,
+        @Path("followType") followType: FollowType
+    ): List<UserDto>
+}
