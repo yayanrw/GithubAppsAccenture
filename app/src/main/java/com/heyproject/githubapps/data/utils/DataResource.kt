44891947c4d52dc1363e5b3940c0e04e442b45a8
yayanrw <yayanraw@gmail.com>
2022-11-11 00:22:14 +0700

    remake DataSource

--- app/src/main/java/com/heyproject/githubapps/data/utils/DataResource.kt
+++ app/src/main/java/com/heyproject/githubapps/data/utils/DataResource.kt
@@ -5,8 +5,8 @@ Written by Yayan Rahmat Wijaya on 11/10/2022 23:42
 Github : https://github.com/yayanrw
  **/
 
-sealed class DataResource<T>(val data: T? = null, val message: String? = null) {
-    class Loading<T>(data: T? = null) : DataResource<T>(data)
-    class Success<T>(data: T?) : DataResource<T>(data)
-    class Error<T>(message: String, data: T? = null) : DataResource<T>(data, message)
+sealed class DataResource<out R> {
+    data class Success<out T>(val data: T) : DataResource<T>()
+    data class Error(val errorMessage: String) : DataResource<Nothing>()
+    object Empty : DataResource<Nothing>()
 }
