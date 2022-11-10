820a24d7bad6f21f5cb615f8fabb73a1c4dbfbbd
yayanrw <yayanraw@gmail.com>
2022-11-11 01:18:00 +0700

    add UserDao.kt

---
+++ app/src/main/java/com/heyproject/githubapps/data/datasource/local/dao/UserDao.kt
@@ -0,0 +1,29 @@
+package com.heyproject.githubapps.data.datasource.local.dao
+
+import androidx.paging.PagingSource
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.heyproject.githubapps.data.datasource.local.entity.UserEntity
+import kotlinx.coroutines.flow.Flow
+
+/**
+Written by Yayan Rahmat Wijaya on 11/11/2022 00:40
+Github : https://github.com/yayanrw
+ **/
+
+@Dao
+interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUser(users: List<UserEntity>)
+
+    @Query("SELECT * FROM user")
+    fun getUsers(): PagingSource<Int, UserEntity>
+
+    @Query("DELETE FROM user")
+    fun deleteAll()
+
+    @Query("SELECT * FROM user WHERE login LIKE '%' || :userName || '%' OR id LIKE '%' || :id || '%'")
+    fun getUsersBy(id: Int, userName: String): Flow<List<UserEntity>>
+}
