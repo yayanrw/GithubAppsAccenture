f56fd9a69fcbef23545d6135a970b57f04065906
yayanrw <yayanraw@gmail.com>
2022-11-10 23:22:24 +0700

    add FollowType.kt

---
+++ app/src/main/java/com/heyproject/githubapps/common/FollowType.kt
@@ -0,0 +1,11 @@
+package com.heyproject.githubapps.common
+
+/**
+Written by Yayan Rahmat Wijaya on 11/10/2022 23:16
+Github : https://github.com/yayanrw
+ **/
+
+enum class FollowType(val followName: String) {
+    FOLLOWING("following"),
+    FOLLOWERS("followers")
+}
