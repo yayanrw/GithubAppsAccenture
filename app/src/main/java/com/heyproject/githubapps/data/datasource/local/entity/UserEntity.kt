d29ca03e911d263eb045309c966663486bccc27d
yayanrw <yayanraw@gmail.com>
2022-11-11 00:56:11 +0700

    add UserEntity.kt

---
+++ app/src/main/java/com/heyproject/githubapps/data/datasource/local/entity/UserEntity.kt
@@ -0,0 +1,18 @@
+package com.heyproject.githubapps.data.datasource.local.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+Written by Yayan Rahmat Wijaya on 11/11/2022 00:42
+Github : https://github.com/yayanrw
+ **/
+
+@Entity(tableName = "user")
+data class UserEntity(
+    @PrimaryKey val id: Int,
+    val login: String,
+    @ColumnInfo(name = "avatar_url") val avatarUrl: String?,
+    val type: String?,
+)
