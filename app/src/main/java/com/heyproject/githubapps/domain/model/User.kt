5c50db00b8648bf78e44f5cc1c7cc69f18efef7f
yayanrw <yayanraw@gmail.com>
2022-11-12 16:02:08 +0700

    add mapper

--- app/src/main/java/com/heyproject/githubapps/domain/model/User.kt
+++ app/src/main/java/com/heyproject/githubapps/domain/model/User.kt
@@ -1,5 +1,8 @@
 package com.heyproject.githubapps.domain.model
 
+import com.heyproject.githubapps.data.datasource.local.entity.UserEntity
+import com.heyproject.githubapps.data.datasource.remote.dto.UserDto
+
 /**
 Written by Yayan Rahmat Wijaya on 11/11/2022 13:31
 Github : https://github.com/yayanrw
@@ -11,3 +14,7 @@ data class User(
     val avatarUrl: String,
     val type: String,
 )
+
+fun UserDto.toEntity(): UserEntity {
+    return UserEntity(id = id, login = login, avatarUrl = avatarUrl, type = type)
+}
