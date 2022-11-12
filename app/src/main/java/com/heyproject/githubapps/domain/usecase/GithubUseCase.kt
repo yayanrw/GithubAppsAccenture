8e5c59fbe71281d0c5f7a4dfa0fc92ffe325c23e
yayanrw <yayanraw@gmail.com>
2022-11-13 04:56:12 +0700

    init usecase

---
+++ app/src/main/java/com/heyproject/githubapps/domain/usecase/GithubUseCase.kt
@@ -0,0 +1,10 @@
+package com.heyproject.githubapps.domain.usecase
+
+/**
+Written by Yayan Rahmat Wijaya on 11/12/2022 16:39
+Github : https://github.com/yayanrw
+ **/
+
+interface GithubUseCase {
+    fun getUsers()
+}
