32f2c1265ac4953f27af3a4b85f464c1c6cc7a30
yayanrw <yayanraw@gmail.com>
2022-11-13 06:08:43 +0700

    add GithubUseCase.kt

--- app/src/main/java/com/heyproject/githubapps/domain/usecase/GithubUseCase.kt
+++ app/src/main/java/com/heyproject/githubapps/domain/usecase/GithubUseCase.kt
@@ -1,10 +1,26 @@
 package com.heyproject.githubapps.domain.usecase
 
+import androidx.lifecycle.LiveData
+import androidx.paging.PagingData
+import com.heyproject.githubapps.common.ViewResource
+import com.heyproject.githubapps.domain.model.User
+import com.heyproject.githubapps.domain.model.UserDetail
+import kotlinx.coroutines.flow.Flow
+
 /**
 Written by Yayan Rahmat Wijaya on 11/12/2022 16:39
 Github : https://github.com/yayanrw
  **/
 
 interface GithubUseCase {
-    fun getUsers()
+    fun getUsers(): LiveData<PagingData<User>>
+    fun getFavoriteUsers(): Flow<List<User>>
+    fun getUserDetail(login: String): Flow<ViewResource<UserDetail>>
+    fun updateUser(user: User, state: Boolean)
+    suspend fun insertUserDetail(userDetail: UserDetail)
+    suspend fun deleteUsers()
+    suspend fun deleteUserDetail()
+    fun searchUsers(query: String): Flow<ViewResource<List<User>>>
+    suspend fun getFollowers(login: String): Flow<ViewResource<List<User>>>
+    suspend fun getFollowings(login: String): Flow<ViewResource<List<User>>>
 }
