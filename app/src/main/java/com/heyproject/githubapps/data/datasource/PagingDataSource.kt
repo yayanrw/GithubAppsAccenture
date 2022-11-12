6b7e5be9e3350dfcae9c6dbe902380bdf5668271
yayanrw <yayanraw@gmail.com>
2022-11-12 17:39:06 +0700

    add PagingDataSource.kt

---
+++ app/src/main/java/com/heyproject/githubapps/data/datasource/PagingDataSource.kt
@@ -0,0 +1,32 @@
+package com.heyproject.githubapps.data.datasource
+
+import androidx.lifecycle.LiveData
+import androidx.paging.*
+import com.heyproject.githubapps.BuildConfig
+import com.heyproject.githubapps.data.datasource.local.database.GithubDatabase
+import com.heyproject.githubapps.data.datasource.local.entity.UserEntity
+import com.heyproject.githubapps.data.datasource.remote.api.GithubService
+
+/**
+Written by Yayan Rahmat Wijaya on 11/12/2022 17:27
+Github : https://github.com/yayanrw
+ **/
+
+class PagingDataSourceImpl(
+    private val githubDatabase: GithubDatabase,
+    private val githubService: GithubService,
+) : PagingDataSource {
+    override fun getUsers(): LiveData<PagingData<UserEntity>> {
+        @OptIn(ExperimentalPagingApi::class) return Pager(config = PagingConfig(pageSize = 10),
+            remoteMediator = UsersRemoteMediator(
+                githubDatabase, githubService, BuildConfig.API_KEY
+            ),
+            pagingSourceFactory = {
+                githubDatabase.userDao().getUsers()
+            }).liveData
+    }
+}
+
+interface PagingDataSource {
+    fun getUsers(): LiveData<PagingData<UserEntity>>
+}
