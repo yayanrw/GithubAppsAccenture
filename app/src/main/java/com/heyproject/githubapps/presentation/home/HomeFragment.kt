33d1e8a1f5f14988ec95e61d81d915cc9a1b0d81
yayanrw <yayanraw@gmail.com>
2022-11-13 19:22:47 +0700

    home fragment

--- app/src/main/java/com/heyproject/githubapps/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/heyproject/githubapps/presentation/home/HomeFragment.kt
@@ -1,14 +1,18 @@
 package com.heyproject.githubapps.presentation.home
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.heyproject.githubapps.R
+import androidx.fragment.app.Fragment
+import com.heyproject.githubapps.databinding.FragmentHomeBinding
+import com.heyproject.githubapps.presentation.adapter.UserAdapter
 
 class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var userAdapter: UserAdapter
 
     companion object {
         fun newInstance() = HomeFragment()
@@ -17,16 +21,31 @@ class HomeFragment : Fragment() {
     private lateinit var viewModel: HomeViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = viewModel
+            homeFragment = this@HomeFragment
+            rvGithubUsers.adapter = UserAdapter()
+            rvGithubUsers.setHasFixedSize(true)
+        }
+
+        setObserver()
+        fetchUsers()
     }
 
+    private fun fetchUsers() {
+        TODO("Not yet implemented")
+    }
+
+    private fun setObserver() {
+        TODO("Not yet implemented")
+    }
 }
--- app/src/main/java/com/heyproject/githubapps/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/heyproject/githubapps/presentation/home/HomeViewModel.kt
@@ -1,7 +1,15 @@
 package com.heyproject.githubapps.presentation.home
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.heyproject.githubapps.domain.model.User
+import com.heyproject.githubapps.domain.usecase.GithubUseCase
+import javax.inject.Singleton
 
-class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@Singleton
+class HomeViewModel(private val githubUseCase: GithubUseCase) : ViewModel() {
+    fun fetchUsers(): LiveData<PagingData<User>> = githubUseCase.getUsers().cachedIn(viewModelScope)
 }
