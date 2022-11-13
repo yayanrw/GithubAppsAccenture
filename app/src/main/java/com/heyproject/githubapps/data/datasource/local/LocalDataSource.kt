7dd016757a2e8dc2d6796c5826e971cc36a7abaa
yayanrw <yayanraw@gmail.com>
2022-11-13 07:09:45 +0700

    inject constructor LocalDataSource.kt

--- app/src/main/java/com/heyproject/githubapps/data/datasource/local/LocalDataSource.kt
+++ app/src/main/java/com/heyproject/githubapps/data/datasource/local/LocalDataSource.kt
@@ -5,20 +5,21 @@ import com.heyproject.githubapps.data.datasource.local.dao.UserDetailDao
 import com.heyproject.githubapps.data.datasource.local.entity.UserDetailEntity
 import com.heyproject.githubapps.data.datasource.local.entity.UserEntity
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
 /**
 Written by Yayan Rahmat Wijaya on 11/11/2022 08:23
 Github : https://github.com/yayanrw
  **/
 
-class LocalDataSourceImpl(private val userDao: UserDao, private val userDetailDao: UserDetailDao) :
-    LocalDataSource {
+class LocalDataSourceImpl @Inject constructor(
+    private val userDao: UserDao, private val userDetailDao: UserDetailDao,
+) : LocalDataSource {
     override suspend fun insertUser(user: UserEntity) = userDao.insertUser(user)
 
     override suspend fun deleteUsers() = userDao.deleteUsers()
 
-    override fun searchUsers(query: String): Flow<List<UserEntity>> =
-        userDao.searchUser(query)
+    override fun searchUsers(query: String): Flow<List<UserEntity>> = userDao.searchUser(query)
 
     override suspend fun insertUserDetail(user: UserDetailEntity) =
         userDetailDao.insertUserDetail(user)
