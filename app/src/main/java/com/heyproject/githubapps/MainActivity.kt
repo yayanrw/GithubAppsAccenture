cc1a727421b7fc5ae451ba194c671f551da26fac
yayanrw <yayanraw@gmail.com>
2022-11-13 15:30:37 +0700

    add navcontainer

--- app/src/main/java/com/heyproject/githubapps/MainActivity.kt
+++ app/src/main/java/com/heyproject/githubapps/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.heyproject.githubapps
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.heyproject.githubapps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setActionBar()
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp() || navController.navigateUp()
+    }
+
+    private fun setActionBar() {
+        supportActionBar?.apply {
+            elevation = 0.0F
+        }
     }
 }
