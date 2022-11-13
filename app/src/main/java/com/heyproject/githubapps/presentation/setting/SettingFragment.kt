457d7c33e5da75335b6dc06c09043353d8ecbfa1
yayanrw <yayanraw@gmail.com>
2022-11-14 00:39:14 +0700

    init fragment_setting.xml

--- app/src/main/java/com/heyproject/githubapps/presentation/setting/SettingFragment.kt
+++ app/src/main/java/com/heyproject/githubapps/presentation/setting/SettingFragment.kt
@@ -5,20 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.heyproject.githubapps.R
+import androidx.fragment.app.viewModels
+import com.heyproject.githubapps.databinding.FragmentSettingBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SettingFragment : Fragment() {
+    private val viewModel: SettingViewModel by viewModels()
 
-    companion object {
-        fun newInstance() = SettingFragment()
-    }
-
-    private lateinit var viewModel: SettingViewModel
+    private var _binding: FragmentSettingBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+        _binding = FragmentSettingBinding.inflate(
+            inflater, container, false
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = viewModel
+            settingFragment = this@SettingFragment
+        }
     }
 }
