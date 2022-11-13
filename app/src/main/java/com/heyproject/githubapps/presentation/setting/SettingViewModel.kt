d267f691807d18aa83f44cf593eeccfa6b45ade2
yayanrw <yayanraw@gmail.com>
2022-11-14 01:08:25 +0700

    add SettingViewModel.kt

--- app/src/main/java/com/heyproject/githubapps/presentation/setting/SettingViewModel.kt
+++ app/src/main/java/com/heyproject/githubapps/presentation/setting/SettingViewModel.kt
@@ -1,7 +1,25 @@
 package com.heyproject.githubapps.presentation.setting
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.heyproject.githubapps.domain.usecase.GithubUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class SettingViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class SettingViewModel @Inject constructor(
+    private val githubUseCase: GithubUseCase
+) : ViewModel() {
+    fun getThemeSettings(): LiveData<Boolean> {
+        return githubUseCase.getThemeSetting().asLiveData()
+    }
+
+    fun saveThemeSettings(isDarkModeActive: Boolean) {
+        viewModelScope.launch {
+            githubUseCase.saveThemeSetting(isDarkModeActive)
+        }
+    }
 }
