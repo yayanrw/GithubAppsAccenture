9125fc5b2756701a24fc461faff3d0d4ae566f01
yayanrw <yayanraw@gmail.com>
2022-11-13 07:08:13 +0700

    add NetworkModule.kt

---
+++ app/src/main/java/com/heyproject/githubapps/di/NetworkModule.kt
@@ -0,0 +1,32 @@
+package com.heyproject.githubapps.di
+
+import com.heyproject.githubapps.data.datasource.remote.api.GithubService
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+/**
+Written by Yayan Rahmat Wijaya on 11/13/2022 07:06
+Github : https://github.com/yayanrw
+ **/
+
+@Module
+class NetworkModule {
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(120, TimeUnit.SECONDS).readTimeout(120, TimeUnit.SECONDS).build()
+    }
+
+    @Provides
+    fun provideApiService(client: OkHttpClient): GithubService {
+        val retrofit = Retrofit.Builder().baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create()).client(client).build()
+        return retrofit.create(GithubService::class.java)
+    }
+}
