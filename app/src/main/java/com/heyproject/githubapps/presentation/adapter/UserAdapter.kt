7d0152859ebf9d595f5f546b6afc10a40cecece2
yayanrw <yayanraw@gmail.com>
2022-11-13 16:18:07 +0700

    add UserAdapter.kt

---
+++ app/src/main/java/com/heyproject/githubapps/presentation/adapter/UserAdapter.kt
@@ -0,0 +1,58 @@
+package com.heyproject.githubapps.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.heyproject.githubapps.databinding.ItemUserBinding
+import com.heyproject.githubapps.domain.model.User
+
+/**
+Written by Yayan Rahmat Wijaya on 11/13/2022 15:58
+Github : https://github.com/yayanrw
+ **/
+
+class UserAdapter : PagingDataAdapter<User, UserAdapter.UserViewHolder>(DiffCallback) {
+    var onItemClick: ((User) -> Unit)? = null
+
+    inner class UserViewHolder(private var binding: ItemUserBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(user: User?) {
+            binding.apply {
+                imgAvatar
+                imgUrl = user?.avatarUrl
+                tvUsername.text = user?.login
+                tvType.text = user?.type
+                executePendingBindings()
+            }
+        }
+
+        init {
+            binding.root.setOnClickListener {
+                onItemClick?.invoke(getItem(bindingAdapterPosition)!!)
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val user = getItem(position)
+        holder.bind(user)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val binding = ItemUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return UserViewHolder(binding)
+    }
+
+    companion object DiffCallback : DiffUtil.ItemCallback<User>() {
+        override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem.id == newItem.id
+        }
+    }
+
+}
