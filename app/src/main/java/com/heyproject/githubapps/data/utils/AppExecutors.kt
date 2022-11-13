220166dbd8622f17ecf516c3f774853a942c8432
yayanrw <yayanraw@gmail.com>
2022-11-13 07:14:01 +0700

    AppExecutors.kt enhance

--- app/src/main/java/com/heyproject/githubapps/data/utils/AppExecutors.kt
+++ app/src/main/java/com/heyproject/githubapps/data/utils/AppExecutors.kt
@@ -1,8 +1,11 @@
 package com.heyproject.githubapps.data.utils
 
+import android.os.Handler
+import android.os.Looper
 import androidx.annotation.VisibleForTesting
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
+import javax.inject.Inject
 
 /**
 Written by Yayan Rahmat Wijaya on 11/12/2022 17:15
@@ -10,11 +13,30 @@ Github : https://github.com/yayanrw
  **/
 
 class AppExecutors @VisibleForTesting constructor(
-    private val diskIO: Executor
+    private val diskIO: Executor, private val networkIO: Executor, private val mainThread: Executor,
 ) {
+    @Inject
     constructor() : this(
-        Executors.newSingleThreadExecutor()
+        Executors.newSingleThreadExecutor(),
+        Executors.newFixedThreadPool(THREAD_COUNT),
+        MainThreadExecutor()
     )
 
     fun diskIO(): Executor = diskIO
+
+    fun networkIO(): Executor = networkIO
+
+    fun mainThread(): Executor = mainThread
+
+    private class MainThreadExecutor : Executor {
+        private val mainThreadHandler = Handler(Looper.getMainLooper())
+
+        override fun execute(command: Runnable) {
+            mainThreadHandler.post(command)
+        }
+    }
+
+    companion object {
+        private const val THREAD_COUNT = 3
+    }
 }
