66d8a04c84c35c05b9c86bc260d35fb3d2479a1e
yayanrw <yayanraw@gmail.com>
2022-11-15 05:52:38 +0700

    SearchViewModelTest.kt testcase

---
+++ app/src/test/java/com/heyproject/githubapps/presentation/favorite/FavoriteViewModelTest.kt
@@ -0,0 +1,17 @@
+package com.heyproject.githubapps.presentation.favorite
+
+import org.junit.Assert.*
+
+import org.junit.Before
+
+/**
+ * Written by Yayan Rahmat Wijaya on 11/15/2022 05:43
+ * Github : https://github.com/yayanrw
+ */
+
+class FavoriteViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+}
