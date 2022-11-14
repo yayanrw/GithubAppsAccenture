b385a3b7c5f4d08c25892ddebb88979982e13e7e
yayanrw <yayanraw@gmail.com>
2022-11-15 03:18:57 +0700

    obfuscate

--- app/src/main/java/com/heyproject/githubapps/di/NetworkModule.kt
+++ app/src/main/java/com/heyproject/githubapps/di/NetworkModule.kt
@@ -1,10 +1,12 @@
 package com.heyproject.githubapps.di
 
+import com.heyproject.githubapps.BuildConfig
 import com.heyproject.githubapps.data.datasource.remote.api.GithubService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -19,17 +21,28 @@ Github : https://github.com/yayanrw
 @Module
 @InstallIn(SingletonComponent::class)
 class NetworkModule {
+    private val certificatePinner =
+        CertificatePinner.Builder().add(HOST_NAME, BuildConfig.SERTIFICATE_KEY_1)
+            .add(HOST_NAME, BuildConfig.SERTIFICATE_KEY_2).build()
+
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
         return OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-            .connectTimeout(120, TimeUnit.SECONDS).readTimeout(120, TimeUnit.SECONDS).build()
+            .connectTimeout(120, TimeUnit.SECONDS).readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner).build()
     }
 
     @Provides
     fun provideApiService(client: OkHttpClient): GithubService {
-        val retrofit = Retrofit.Builder().baseUrl("https://api.github.com/")
-            .addConverterFactory(GsonConverterFactory.create()).client(client).build()
+        val retrofit =
+            Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
+                .client(client).build()
         return retrofit.create(GithubService::class.java)
+    }
+
+    companion object {
+        const val HOST_NAME = "api.github.com"
+        const val BASE_URL = "https://api.github.com/"
     }
 }
