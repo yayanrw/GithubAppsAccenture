24bb3809b4bad83e8dd0562d74ec648d42163185
yayanrw <yayanraw@gmail.com>
2022-11-14 21:54:14 +0700

    init FavoriteFragment.kt

--- app/src/main/java/com/heyproject/githubapps/presentation/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/heyproject/githubapps/presentation/favorite/FavoriteFragment.kt
@@ -5,21 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.heyproject.githubapps.R
+import androidx.fragment.app.viewModels
+import com.heyproject.githubapps.databinding.FragmentFavoriteBinding
 
 class FavoriteFragment : Fragment() {
+    private val viewModel: FavoriteViewModel by viewModels()
 
-    companion object {
-        fun newInstance() = FavoriteFragment()
-    }
-
-    private lateinit var viewModel: FavoriteViewModel
+    private var _binding: FragmentFavoriteBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
