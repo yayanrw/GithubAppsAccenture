18fee7c31fe96595414d311b04ce05dcdb29c55c
yayanrw <yayanraw@gmail.com>
2022-11-14 18:48:49 +0700

    search view

--- app/src/main/java/com/heyproject/githubapps/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/heyproject/githubapps/presentation/search/SearchFragment.kt
@@ -1,26 +1,66 @@
 package com.heyproject.githubapps.presentation.search
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.view.*
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import com.heyproject.githubapps.R
+import com.heyproject.githubapps.databinding.FragmentSearchBinding
+import com.heyproject.githubapps.presentation.adapter.FollowAdapter
+import dagger.hilt.android.AndroidEntryPoint
 
-class SearchFragment : Fragment() {
+@AndroidEntryPoint
+class SearchFragment : Fragment(), MenuProvider, SearchView.OnQueryTextListener {
+    private val viewModel: SearchViewModel by viewModels()
 
-    companion object {
-        fun newInstance() = SearchFragment()
-    }
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
 
-    private lateinit var viewModel: SearchViewModel
+    private lateinit var followAdapter: FollowAdapter
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.search_menu, menu)
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+        searchView.queryHint = getString(R.string.search)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return when (menuItem.itemId) {
+            R.id.action_search -> {
+
+                true
+            }
+            else -> {
+                true
+            }
+        }
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        return false
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        return true
+    }
 }
